d352a98acfada8eaab8a16989fb6614d19bf7ea0
Kevin Liao <kliao14@gmail.com>
2024-08-19 16:22:54 -0700

    auth adapter setup

--- src/auth.ts
+++ src/auth.ts
@@ -1,32 +1,30 @@
 import NextAuth, { DefaultSession } from "next-auth";
 import Google from "next-auth/providers/google";
 import type { Provider } from "next-auth/providers";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "@/schema";
+import authConfig from "@/auth.config";
 
 const providers: Provider[] = [Google];
 
-declare module "next-auth" {
-  interface Session {
-    user: {
-      id: string;
-    } & DefaultSession["user"];
-  }
-}
-
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers,
-  callbacks: {
-    jwt({ token, user, account }) {
-      if (user) {
-        // User is available during sign-in
-        token.id = account?.providerAccountId;
-      }
-      return token;
-    },
-    session({ session, token }) {
-      session.user.id = token.id as string;
-      return session;
-    },
-  },
+  debug: true,
+  adapter: DrizzleAdapter(db),
+
+  // providers,
+  // callbacks: {
+  //   jwt({ token, user, account }) {
+  //     if (user) {
+  //       // User is available during sign-in
+  //       token.id = account?.providerAccountId;
+  //     }
+  //     return token;
+  //   },
+  //   session({ session, token }) {
+  //     session.user.id = token.id as string;
+  //     return session;
+  //   },
+  // },
   // callbacks: {
   //   async session({ session, user }) {
   //     session.user.id = user.id;
@@ -36,13 +34,14 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   // pages: {
   //   signIn: "/signin",
   // },
+  ...authConfig,
 });
 
-export const providerMap = providers.map((provider) => {
-  if (typeof provider === "function") {
-    const providerData = provider();
-    return { id: providerData.id, name: providerData.name };
-  } else {
-    return { id: provider.id, name: provider.name };
-  }
-});
+// export const providerMap = providers.map((provider) => {
+//   if (typeof provider === "function") {
+//     const providerData = provider();
+//     return { id: providerData.id, name: providerData.name };
+//   } else {
+//     return { id: provider.id, name: provider.name };
+//   }
+// });
