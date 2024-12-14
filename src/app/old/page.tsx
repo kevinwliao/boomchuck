eb8e5244874359638797fe08bd6abafaa0f63d51
Kevin Liao <kliao14@gmail.com>
2024-12-13 16:22:19 -0800

    deletes

--- src/app/old/page.tsx
+++
@@ -1,6 +0,0 @@
-// import BoomChuck from "@/components/boomChuck";
-
-export default function Page() {
-  return <></>;
-  // return <BoomChuck></BoomChuck>;
-}
--- src/app/tuner/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <main className="flex grow items-center justify-center">
-      <div className="mx-auto text-4xl font-semibold leading-tight">
-        Nothing to see here...
-      </div>
-    </main>
-  );
-}
